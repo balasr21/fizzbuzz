7ec597fc00295b414741c8fe193cc621e9b44982
Bala <bala.murugan@mnscorp.net>
2020-03-25 17:51:00 +0000

    fizzBuzz converter and junit added

--- src/main/java/com/techjava/fizzbuzz/FizzBuzz.java
+++ src/main/java/com/techjava/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,36 @@
 package com.techjava.fizzbuzz;
 
+/**
+ * The class FizzBuzz.
+ *
+ */
 public class FizzBuzz {
+
+    /**
+     * converter - This method converts value to fizzbuzz
+     *
+     * If the given value is a multiple of 3 ,"Fizz" will be returned
+     * If the given value is a multiple of 5,"Buzz" will be returned
+     * If the given value is a multiple of 3 & 5,"FizzBuzz" will be returned
+     *
+     * @param value
+     * @return
+     */
+    public String converter(Integer value){
+
+        boolean isFizz=(value%3==0);
+        boolean isBuzz=(value%5==0);
+        boolean isFizzBuzz=(value%15==0);
+
+        if(isFizzBuzz){
+            return "FizzBuzz";
+        }else if(isFizz){
+            return "Fizz";
+        }else if(isBuzz){
+            return "Buzz";
+        }else{
+            return String.valueOf(value);
+        }
+    }
 
 }
--- src/test/java/com/techjava/fizzbuzztest/FizzBuzzTester.java
+++ src/test/java/com/techjava/fizzbuzztest/FizzBuzzTester.java
@@ -1,4 +1,98 @@
 package com.techjava.fizzbuzztest;
 
+
+import com.techjava.fizzbuzz.FizzBuzz;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The Class FizzBuzzTester.
+ */
 public class FizzBuzzTester {
+
+    FizzBuzz fizzBuzz=new FizzBuzz();
+
+    static List<Integer> multipleOf3;
+    static List<Integer> multipleOf5;
+    static List<Integer> commonMultiple;
+    static List<Integer> restAllNumbers;
+
+    @BeforeAll
+    static void setup(){
+
+
+        multipleOf3=getMultiples(1,100,3);
+        multipleOf5=getMultiples(1,100,5);
+        restAllNumbers=getMultiples(1,100,1);
+
+        commonMultiple=new ArrayList<>(multipleOf3);
+        commonMultiple.retainAll(multipleOf5);
+
+
+        multipleOf3.removeAll(commonMultiple);
+        multipleOf5.removeAll(commonMultiple);
+        restAllNumbers.removeAll(commonMultiple);
+        restAllNumbers.removeAll(multipleOf5);
+        restAllNumbers.removeAll(multipleOf3);
+
+    }
+
+    static List<Integer> getMultiples(int min,int max,int multiple){
+        return IntStream.rangeClosed(min,max).filter(m->m%multiple==0).boxed().collect(Collectors.toList());
+    }
+
+
+    @DisplayName("Test sample input numbers")
+    @Test
+    public void isConverterValidForOtherSampleNumbers(){
+
+        assertEquals("1",fizzBuzz.converter(1));
+        assertEquals("-1",fizzBuzz.converter(-1));
+        assertEquals("98",fizzBuzz.converter(98));
+    }
+
+
+    @DisplayName("Test rest of numbers which are not multiples of 3,5")
+    @Test
+    public void isConverterValidForAllRestNumbers(){
+       for(Integer n:restAllNumbers){
+           assertEquals(String.valueOf(n),fizzBuzz.converter(n));
+       }
+    }
+
+    @DisplayName("Test multiples of 3 for fizz")
+    @Test
+    public void isConverterValidForFizzNumbers(){
+        for(Integer n:multipleOf3){
+            assertEquals("Fizz",fizzBuzz.converter(n));
+        }
+    }
+
+    @DisplayName("Test multiples of 5 for buzz")
+    @Test
+    public void isConverterValidForBuzzNumbers(){
+        for(Integer n:multipleOf5){
+            assertEquals("Buzz",fizzBuzz.converter(n));
+        }
+    }
+
+    @DisplayName("Test common multiples of 3 & 5 for fizzbuzz")
+    @Test
+    public void isConverterValidForFizzBuzzNumbers(){
+        for(Integer n:commonMultiple){
+            assertEquals("FizzBuzz",fizzBuzz.converter(n));
+        }
+    }
+
+
+
+
 }
